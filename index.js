64d3673317514caccde8d0c0b1c071ca4ae681d4
Md Suny Shaikh <mdsunyshaikh@gmail.com>
2024-06-03 23:08:49 +0600

    push apartments data to db and get data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require("mongodb")
 
 require("dotenv").config()
 
@@ -12,6 +13,39 @@ app.use(cors())
 app.get("/", (req, res) => {
   res.send("Welcome to Taj Apart")
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y3rtmj6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+async function run() {
+  try {
+    const apartmentCollection = client.db("tajApart").collection("apartments")
+
+    // Apartments
+    app.get("/apartments", async (req, res) => {
+      const apartments = await apartmentCollection.find().toArray()
+      res.send(apartments)
+    })
+
+    app.patch("/apartments/:id", async (req, res) => {})
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 })
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
