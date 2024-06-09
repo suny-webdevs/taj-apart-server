0b49796a9c9dcf7274c5d377a89e17deacc885f4
Md Suny Shaikh <mdsunyshaikh@gmail.com>
2024-06-09 12:26:06 +0600

    get user, put user and get user role api build

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://taj-apart.web.app",
+      "https://taj-apart.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+)
 
 app.get("/", (req, res) => {
   res.send("Welcome to Taj Apart")
@@ -26,6 +35,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const apartmentCollection = client.db("tajApart").collection("apartments")
+    const userCollection = client.db("tajApart").collection("users")
+    const agreementCollection = client.db("tajApart").collection("agreements")
 
     // Apartments
     app.get("/apartments", async (req, res) => {
@@ -34,6 +45,101 @@ async function run() {
     })
 
     app.patch("/apartments/:id", async (req, res) => {})
+
+    // Apartments for pagination
+    app.get("/all-apartments", async (req, res) => {
+      const page = parseInt(req.query.page) - 1
+      const size = parseInt(req.query.size)
+
+      const result = await apartmentCollection
+        .find()
+        .skip(page * size)
+        .limit(size)
+        .toArray()
+      res.send(result)
+    })
+
+    app.get("/apartments-count", async (req, res) => {
+      const count = await apartmentCollection.countDocuments()
+      res.send({ count })
+    })
+
+    // Users
+    app.get("/users", async (req, res) => {
+      const users = await userCollection.find().toArray()
+      res.send(users)
+    })
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email
+      const query = { email }
+      const result = await userCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.put("/users", async (req, res) => {
+      const user = req.body
+
+      const query = { email: user?.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        if (user.role === "member") {
+          const result = await userCollection.updateOne(query, {
+            $set: { role: user?.role },
+          })
+          return res.send(result)
+        }
+      } else {
+        return res.send(existingUser)
+      }
+
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          ...user,
+        },
+      }
+
+      const postUser = await userCollection.updateOne(query, updateDoc, options)
+      res.send(postUser)
+    })
+
+    // Agreements
+    app.get("/agreements", async (req, res) => {
+      const agreements = await agreementCollection.find().toArray()
+      res.send(agreements)
+    })
+
+    app.put("/agreements", async (req, res) => {
+      const agreement = req.body
+
+      const query = {
+        apartment_no: agreement.apartment_no,
+      }
+
+      const currentUser = await userCollection.findOne({
+        email: agreement?.user.email,
+      })
+      if (currentUser) {
+        const existingAgreement = await agreementCollection.findOne(query)
+        if (existingAgreement) return res.send(existingAgreement)
+      } else {
+      }
+
+      const option = { upsert: true }
+      const updateDoc = {
+        $set: {
+          ...agreement,
+        },
+      }
+
+      const postAgreement = await agreementCollection.updateOne(
+        query,
+        updateDoc,
+        option
+      )
+      res.send(postAgreement)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 })
